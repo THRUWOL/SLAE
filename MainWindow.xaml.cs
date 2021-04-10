4004446b438c1bd77f0e9f32f4f0d3d29b478f27
Nikita Yarosh <ThRuWol@yandex.ru>
2021-04-10 16:56:09 +0300

    New Seidel method

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -126,6 +126,25 @@ namespace SLAE
             a33.Text = Convert.ToString(rnd.Next(-100, 100));
             b3.Text = Convert.ToString(rnd.Next(-100, 100));
         }
+        // Заполнение текстбокса ответом метода Зейделя
+        private void AddAdditional(double[] answer)
+        {
+            a11.Text = "";
+            a12.Text = "";
+            a13.Text = "";
+            //b1.Text = Convert.ToString(Math.Round(answer[0], 5));
+            b1.Text = Convert.ToString(answer[0]);
+            a21.Text = "";
+            a22.Text = "";
+            a23.Text = "";
+            //b2.Text = Convert.ToString(Math.Round(answer[1], 5));
+            b2.Text = Convert.ToString(answer[1]);
+            a31.Text = "";
+            a32.Text = "";
+            a33.Text = "";
+            //b3.Text = Convert.ToString(Math.Round(answer[2], 5));
+            b3.Text = Convert.ToString(answer[2]);
+        }
         /* ЗОНА ПОВЫШЕННОГО БЫДЛОКОДА ЗАКОНЧЕНА */
 
         static double[,] EOR1(double[,] A, int renglon, double factor)
@@ -151,8 +170,6 @@ namespace SLAE
             }
             return A;
         }
-
-
         private void BStart_Click(object sender, RoutedEventArgs e)
         {
             // Метод Гаусса-Жордана (1 задание)
@@ -177,7 +194,13 @@ namespace SLAE
             // Метод Зейделя (2 задание)
             else if (CBSeidel.IsChecked == true)
             {
-                
+                int numVar = 3;
+                double[,] matrix = CreateMatrix(numVar);
+                double[] addit = CreateAdditional(numVar);
+                GaussSeidel gaussSeidel = new GaussSeidel(matrix, addit, 0.001, false);
+                double[] answer = gaussSeidel.Answer;
+                AddAdditional(answer);
+
             }
             // Метод Тихонова (бонус)
             else if (CBTikhonov.IsChecked == true)
@@ -217,5 +240,77 @@ namespace SLAE
         }
 
     }
+    public class GaussSeidel
+    {
+        public double Epsilon { get; set; }
 
+        public GaussSeidel(double[,] leftPart, double[] rightPart, double epsilon, bool isParallel)
+        {
+            if (Epsilon > 0.1)
+                throw new ArgumentException("epsilon > 0.1", nameof(epsilon));
+
+            Epsilon = epsilon;
+            Answer = new double[rightPart.GetLength(0)];
+            if (isParallel)
+            {
+                TrySolveParallel(leftPart, rightPart);
+                return;
+            }
+
+            TrySolve(leftPart, rightPart);
+        }
+        public long Iterations { get; private set; }
+        public double[] Answer { get; private set; }
+
+        private static bool IsConverge(
+            IReadOnlyList<double> curr,
+            IReadOnlyList<double> prev, int n, double eps)
+        {
+            double norm = 0;
+            for (var i = 0; i < n; i++)
+            {
+                norm += Math.Pow(curr[i] - prev[i], 2);
+            }
+            return !(Math.Sqrt(norm) >= eps);
+        }
+
+        private void TrySolve(double[,] leftPart, IReadOnlyList<double> rightPart)
+        {
+            var curr = new double[rightPart.Count];
+            for (var i = 0; i < curr.Length; i++)
+            {
+                curr[i] = 0;
+            }
+            var prev = new double[rightPart.Count];
+
+            var n = leftPart.GetLength(0);
+            long counter = 0;
+            do
+            {
+                if (counter > 1000000)
+                    return;
+
+                ++counter;
+
+                for (var i = 0; i < n; i++)
+                    prev[i] = curr[i];
+
+                for (var i = 0; i < n; i++)
+                {
+                    double var = 0;
+                    for (var j = 0; j < i; j++)
+                        var += leftPart[i,j] * curr[j];
+                    for (var j = i + 1; j < n; j++)
+                        var += leftPart[i,j] * prev[j];
+                    curr[i] = (rightPart[i] - var) / leftPart[i,i];
+                }
+            } while (!IsConverge(curr, prev, n, Epsilon));
+            Answer = curr;
+            Iterations = counter;
+        }
+
+        private void TrySolveParallel(double[,] leftPart, double[] rightPart)
+        {
+        }
+    }
 }
